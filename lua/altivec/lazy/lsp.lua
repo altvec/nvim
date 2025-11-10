7889c327727bec4785ac39287ede020e464688d6
Sergey Kalistratov <sergey.kalistratov@dualbootpartners.com>
2025-11-10 16:20:13 +0500

    simplify LSP diagnostic

--- lua/altivec/lazy/lsp.lua
+++ lua/altivec/lazy/lsp.lua
@@ -163,33 +163,34 @@ return {
 
 			-- Diagnostic Config
 			-- See :help vim.diagnostic.Opts
-			vim.diagnostic.config({
-                virtual_lines = true,
-				severity_sort = true,
-				float = { border = "rounded", source = "if_many" },
-				underline = { severity = vim.diagnostic.severity.ERROR },
-				signs = vim.g.have_nerd_font and {
-					text = {
-						[vim.diagnostic.severity.ERROR] = "󰅚 ",
-						[vim.diagnostic.severity.WARN] = "󰀪 ",
-						[vim.diagnostic.severity.INFO] = "󰋽 ",
-						[vim.diagnostic.severity.HINT] = "󰌶 ",
-					},
-				} or {},
-				virtual_text = {
-					source = "if_many",
-					spacing = 2,
-					format = function(diagnostic)
-						local diagnostic_message = {
-							[vim.diagnostic.severity.ERROR] = diagnostic.message,
-							[vim.diagnostic.severity.WARN] = diagnostic.message,
-							[vim.diagnostic.severity.INFO] = diagnostic.message,
-							[vim.diagnostic.severity.HINT] = diagnostic.message,
-						}
-						return diagnostic_message[diagnostic.severity]
-					end,
-				},
-			})
+			-- vim.diagnostic.config({
+			--              virtual_lines = true,
+			-- 	severity_sort = true,
+			-- 	float = { border = "rounded", source = "if_many" },
+			-- 	underline = { severity = vim.diagnostic.severity.ERROR },
+			-- 	signs = vim.g.have_nerd_font and {
+			-- 		text = {
+			-- 			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			-- 			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			-- 			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			-- 			[vim.diagnostic.severity.HINT] = "󰌶 ",
+			-- 		},
+			-- 	} or {},
+			-- 	virtual_text = {
+			-- 		source = "if_many",
+			-- 		spacing = 2,
+			-- 		format = function(diagnostic)
+			-- 			local diagnostic_message = {
+			-- 				[vim.diagnostic.severity.ERROR] = diagnostic.message,
+			-- 				[vim.diagnostic.severity.WARN] = diagnostic.message,
+			-- 				[vim.diagnostic.severity.INFO] = diagnostic.message,
+			-- 				[vim.diagnostic.severity.HINT] = diagnostic.message,
+			-- 			}
+			-- 			return diagnostic_message[diagnostic.severity]
+			-- 		end,
+			-- 	},
+			-- })
+            vim.diagnostic.config { virtual_lines = true, virtual_text = false }
 
 			-- LSP servers and clients are able to communicate to each other what features they support.
 			--  By default, Neovim doesn't support everything that is in the LSP specification.
